19565ee61c018c23494bb54ad9634e8d55f6d35f
Hola <hola@Holas-MacBook-Pro.local>
2024-01-13 11:58:11 +0800

    rgrep v2

--- rgrep/src/main.rs
+++ rgrep/src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use regex::Regex;
 use rgrep::args::Args;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,19 +10,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let word = args.word;
     let files = args.files.unwrap_or_default();
 
+    let re = Regex::new(&word).unwrap();
+
     for file in files {
-        grep_file(&word, &file)?;
+        grep_file(&re, &file)?;
     }
 
     Ok(())
 }
 
-fn grep_file(word: &str, file: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn grep_file(re: &Regex, file: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut lines = read_lines(file)?;
     let mut row = 1;
     while let Some(line) = lines.next() {
         let line = line?;
-        if line.contains(word) {
+        if let Some(_) = re.captures(&line) {
             println!("{}: {}", row, line);
         }
         row += 1;
