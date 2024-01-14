c85d8200db5cb11a214a3661d8ae0bb0fd0f7cf6
Hola <hola@Holas-MacBook-Pro.local>
2024-01-14 15:34:39 +0800

    unsafe

--- unsafe-code/src/main.rs
+++ unsafe-code/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let mut s = "我爱你！中国".to_string();
     let r = s.as_mut();
 
-    if let Some((s1, s2)) = split(r, '！') {
+    if let Some((s1, s2)) = split_mut(r, '！') {
         println!("s1: {}, s2: {}", s1, s2);
     }
 }
@@ -17,5 +17,19 @@ fn split(s: &str, sep: char) -> Option<(&str, &str)> {
         // SAFETY: pos 是 find 得到的，它位于字符的边界处，同样 pos + sep_len 也是如此
         // 所以以下代码是安全的
         unsafe { (s.get_unchecked(0..pos), s.get_unchecked(pos + sep_len..len)) }
+    })
+}
+
+fn split_mut(s: &mut str, sep: char) -> Option<(&mut str, &mut str)> {
+    split(s, sep).map(|(s1, s2)| {
+        let (ptr1, len1) = unsafe { std::mem::transmute(s1) };
+        let (ptr2, len2) = unsafe { std::mem::transmute(s2) };
+        let s1 = unsafe {
+            std::str::from_utf8_unchecked_mut(std::slice::from_raw_parts_mut(ptr1, len1))
+        };
+        let s2 = unsafe {
+            std::str::from_utf8_unchecked_mut(std::slice::from_raw_parts_mut(ptr2, len2))
+        };
+        (s1, s2)
     })
 }
