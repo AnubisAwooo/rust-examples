020f3c72c162e078ea04dc1ddd268d7b909fe000
Hola <hola@Holas-MacBook-Pro.local>
2024-01-18 13:46:08 +0800

    macros

---
+++ macros/src/lib.rs
@@ -0,0 +1,24 @@
+use proc_macro::TokenStream;
+
+mod raw_builder;
+
+use raw_builder::BuilderContext;
+
+#[proc_macro]
+pub fn query(input: TokenStream) -> TokenStream {
+    println!("{:#?}", input);
+    "fn hello() { println!(\"Hello world!\"); }"
+        .parse()
+        .unwrap()
+}
+
+// #[proc_macro_derive(RawBuilder)]
+// pub fn derive_raw_builder(input: TokenStream) -> TokenStream {
+//     println!("{:#?}", input);
+//     TokenStream::default()
+// }
+
+#[proc_macro_derive(RawBuilder)]
+pub fn derive_raw_builder(input: TokenStream) -> TokenStream {
+    BuilderContext::render(input).unwrap().parse().unwrap()
+}
